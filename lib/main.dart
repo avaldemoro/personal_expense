c8bedb1604a94fd3ac4305ebf9c513eb57499b56
Aster Valdemoro <astervaldemoro@gmail.com>
2019-12-08 15:50:56 -0500

    added more styles

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 import './transaction.dart';
 
@@ -31,10 +32,17 @@ class MyHomePage extends StatelessWidget {
             amount: 65.95,
             datetime: DateTime.now(),
         ),
+        Transaction(
+            id: 't3',
+            title: 'treats',
+            amount: 5.00,
+            datetime: DateTime.now(),
+        ),
     ];
 
     @override
     Widget build(BuildContext context) {
+        var dateTimeFormatter = new DateFormat("E, MMMM d, y").add_jm();
         return Container(
             padding: EdgeInsets.only(top: 30.0),
             color: Color(0xFF223640),
@@ -74,7 +82,7 @@ class MyHomePage extends StatelessWidget {
                                         margin: EdgeInsets.symmetric(vertical: 38.0),
                                         padding: EdgeInsets.symmetric(horizontal: 8.0),
                                         child: Text(
-                                            'this is where chart would go!',
+                                            'this is where chart would gooooo!',
                                             style: TextStyle(
                                                 color: Colors.white,
                                             ),
@@ -86,16 +94,45 @@ class MyHomePage extends StatelessWidget {
                                 children: transactions.map((tx) {
                                     return Card(
                                         elevation: 5,
-                                        child: Padding(
-                                            padding: EdgeInsets.symmetric(horizontal: 20.0),
-                                            child: Container(
-                                                margin: EdgeInsets.symmetric(vertical: 16.0),
-                                                padding: EdgeInsets.symmetric(horizontal: 8.0),
+                                        child: Row(children: <Widget>[
+                                            Container(
                                                 child: Text(
-                                                    tx.title,
+                                                    '\$ ${tx.amount.toStringAsFixed(2)}',
+                                                    style: TextStyle(
+                                                        fontSize: 18.0,
+                                                        fontWeight: FontWeight.bold,
+                                                    ),
+                                                ),
+                                                padding: EdgeInsets.symmetric(horizontal: 16.0),
+                                            ),
+                                            Container(
+                                                child: Padding(
+                                                    padding: EdgeInsets.symmetric(horizontal: 16.0),
+                                                    child: Container(
+                                                        margin: EdgeInsets.symmetric(vertical: 16.0),
+                                                        padding: EdgeInsets.symmetric(horizontal: 8.0),
+                                                        child: Column(
+                                                            crossAxisAlignment: CrossAxisAlignment.start,
+                                                            children: [
+                                                                Text(
+                                                                    tx.title,
+                                                                    style: TextStyle(
+                                                                        fontSize: 16.0,
+                                                                    ),
+                                                                ),
+                                                                Text(
+                                                                    dateTimeFormatter.format(tx.datetime),
+                                                                    style: TextStyle(
+                                                                        color: Color(0xFFb3b3b3),
+                                                                        fontSize: 12.0,
+                                                                    ),
+                                                                ),
+                                                            ],
+                                                        ),
+                                                    ),
                                                 ),
                                             ),
-                                        ),
+                                        ],)
                                     );
                                 }).toList(),
                             ),
